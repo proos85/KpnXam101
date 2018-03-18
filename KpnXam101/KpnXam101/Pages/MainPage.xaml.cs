f365f735fa7e5894a7d897c927d16f823ed62cfb
Patrick Roos <patrick@point42.nl>
2018-03-18 15:20:34 +0100

    My Simple Calculator Entry

--- KpnXam101/KpnXam101/Pages/MainPage.xaml.cs
+++ KpnXam101/KpnXam101/Pages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.Xaml;
 
 namespace KpnXam101.Pages
 {
@@ -9,7 +11,45 @@ namespace KpnXam101.Pages
 		{
 			InitializeComponent();
 
-		    XamarinMonkey.Source = "https://i.redditmedia.com/92TL_kmRrwXC1eenJG0ocjMrYcNM64_1KtwtW6zo-u8.png?w=320&s=02380754ddf1ea89db0abee08a47f4bd";
+		    Picker.ItemsSource = new List<string>
+		    {
+		        "Plus",
+		        "Min",
+            };
 		}
-	}
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            if (!int.TryParse(Getal1.Text, out int getal1))
+            {
+                await DisplayAlert("Niet geldig", "Voer een geldig getal in", "OK");
+                return;
+            }
+
+            if (!int.TryParse(Getal2.Text, out int getal2))
+            {
+                await DisplayAlert("Niet geldig", "Voer een geldig getal in", "OK");
+                return;
+            }
+
+            string actie = (string) Picker.SelectedItem ?? string.Empty;
+            if (string.IsNullOrEmpty(actie))
+            {
+                await DisplayAlert("Niet geldig", "Voer een geldig actie in", "OK");
+                return;
+            }
+
+            int totaal;
+            if (actie.Equals("plus", StringComparison.InvariantCultureIgnoreCase))
+            {
+                totaal = getal1 + getal2;
+            }
+            else
+            {
+                totaal = getal1 - getal2;
+            }
+
+            await DisplayAlert("Uitkomst", $"{totaal}", "OK");
+        }
+    }
 }
