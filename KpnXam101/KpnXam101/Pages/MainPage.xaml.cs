9ef3d01eecad7cfb3acb15dce8eec157a8b1db1d
Patrick Roos <patrick@point42.nl>
2018-03-18 14:16:20 +0100

    Add project files.

---
+++ KpnXam101/KpnXam101/Pages/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace KpnXam101.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage
+    {
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
